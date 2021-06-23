c61e2f631ce8811f50ee57061f825a6c6ddc3e7e
bhatsy <85468712+bhatsy@users.noreply.github.com>
2021-06-23 22:16:01 +0530

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @NTG_family
+# (c) @SpEcHIDe
 
 from pyrogram import Client, __version__
 
-from . import API_HASH, APP_ID, LOGGER, \
-    BOT_SESSION, BOT_TOKEN 
+from . import API_HASH, APP_ID, LOGGER, BOT_TOKEN 
 
 from .user import User
-
-
 
 class Bot(Client):
     USER: User = None
@@ -17,27 +14,27 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            BOT_SESSION,
+            "bot",
             api_hash=API_HASH,
             api_id=APP_ID,
             plugins={
                 "root": "bot/plugins"
             },
             workers=4,
-            bot_token=BOT_TOKEN
+            bot_token=BOT_TOKEN,
+            sleep_threshold=10
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        usr_bot_me = await self.get_me()
+        bot_details = await self.get_me()
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username}  started! "
+            f"@{bot_details.username}  started! "
         )
         self.USER, self.USER_ID = await User().start()
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped. Bye.")
-©
