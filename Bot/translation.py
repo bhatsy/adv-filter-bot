4f2e15456e7294afb4d6a4d7457dc1396d7508ba
bhatsy <85468712+bhatsy@users.noreply.github.com>
2021-06-23 22:27:12 +0530

    Update translation.py

--- Bot/translation.py
+++ Bot/translation.py
@@ -4,24 +4,55 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hai {}!!</b>
-<i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...
-Just Sent Any Text I Will Search In All Connected Chat And Reply You With The Message link
-You Can Even Connected To 3 Channels At A Time...</i>"""    
-    
-    HELP_TEXT = """<i><u>Usage Guide</u></i>
-<i>=> Add Bot To Any Channel As Admin With Add Members/ Invite Users Via Link
-=> Copy Channel ID
-=> Use <code>/connect {channel id}</code> In Your Group To Connect With The Group
-=> Use <code>/disconnect {channel id}</code> In Your Group To Disconnect From Your Group
-=> Use <code>/delall</code> In Your Group To Clear All Your Group  Connections (Owner Only)
-Now You Are All Set And Ready To Go...
-Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
-"""
+    START_TEXT = """<b>Hey {}!!</b>
+<i>Am Just A Advance Auto Filter Bot....😉
 
-    ABOUT_TEXT = """<b>➥ Name</b> : <code>adv-filter-bot</code>
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/NTG_family">NTG_family</a></i></b>
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+
+For More Details Click Help Button Below..
+@T5links
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i><u>How To Use Me!?</u></i></b>
+
+<i>
+-> Add Me To Any Group And Make Me Admin
+-> Add Me To Your Desired Channel
+</i>
+
+<b>Bot Commands (Works Only In Groups) :</b>
+
+    -> <code>/add chat_id</code>
+                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+     <code>/add @Username</code>
+     
+    -> <code>/del chat_id</code>
+                OR                  - To disconnect A Group With A Channel
+     <code>/del @Username</code>
+     
+    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    
+    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+@T5links
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> adv-filter-bot </code>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/NTG_family">NTG_family</a></i></b>
+
 <b>➥ Language</b> : <code>Python3</code>
-<b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
-<b>➥ Source Code</b> : <i><a herf="https://github.com/bhatsy/adv-filter-bot">GitHub</a></i>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://github.com/bhatsy/adv-filter-bot">Click Me</a></i>
 """
